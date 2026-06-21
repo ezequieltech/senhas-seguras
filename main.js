0a6288aa455a3c3faa24226627de78b9bef84ad2
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2026-06-21 18:55:13 -0300

    f

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 
 numeroSenha.textContent = 5;
 
-const botoes = document.querySelector('.parametro-senha__botao')
+const botoes = document.querySelector('.parametro-senha__botao');
