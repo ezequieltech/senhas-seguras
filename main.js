dfbac4e22faef56de0ad7f28c372a6c09bad65bb
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2026-06-21 19:02:02 -0300

    n

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-
-numeroSenha.textContent = 5;
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-console.log('botoes');
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){
+tamanhoSenha = tamanhoSenha -1;
+}
+console.log(botoes);
